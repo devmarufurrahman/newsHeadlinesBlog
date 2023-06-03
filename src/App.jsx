7a5bbcf57920bf626cd007a493f7a1fd900006b4
marufur-rahman <mdmarufur5682@gmail.com>
2023-06-03 14:26:12 +0600

    header componentnt add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Class/Header";
+import { NewsCategories } from "./utils/Categories";
 
 class App extends React.Component {
-	render(){
-    return <></>;
-  }
+	render() {
+		return (
+			<>
+				<div className="container">
+					<div className="row">
+						<div className="col-sm-6 ">
+							<Header category={NewsCategories.technology} />
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default App;
--- src/components/Class/Header/index.jsx
+++ src/components/Class/Header/index.jsx
@@ -1,35 +1,51 @@
 import React, { Component } from "react";
-import { NewsCategories } from "../News/Categories";
+import { NewsCategories } from "../../../utils/Categories";
 
 class Header extends Component {
 	state = {
 		searchTerm: "",
 	};
 
-	handleChange=e=>{
-
-	}
+	handleChange = (e) => {
+		this.setState({
+			searchTerm: e.target.value,
+		});
+	};
 	render() {
-		const {categorise}=this.props
+		const { category } = this.props;
 		return (
 			<>
-				<div className="my-4">
+				<div className="my-4 text-center">
 					<h1 className="mb-4 font-bold">News Headline</h1>
 					<input
 						type="search"
-						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						className="shadow appearance-none border rounded w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 						placeholder="Type Anything & Press Enter TO Search"
 						value={this.state.searchTerm}
 						onChange={this.handleChange}
 					/>
 					<div className="my-4">
-						{
-							NewsCategories && Object.keys(NewsCategories).map(item=>{
-								if(categorise === NewsCategories[item]){
-									//
+						{NewsCategories &&
+							Object.keys(NewsCategories).map((item) => {
+								if (category === NewsCategories[item]) {
+									return (
+										<button
+											key={NewsCategories[item]}
+											className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-2"
+											type="button">
+											{`#${NewsCategories[item]}`}
+										</button>
+									);
 								}
-							})
-						}
+								return (
+									<button
+										key={NewsCategories[item]}
+										className="bg-gray-200 font-bold py-2 px-4 rounded focus:outline-none mx-2 focus:shadow-outline"
+										type="button">
+										{`#${NewsCategories[item]}`}
+									</button>
+								);
+							})}
 					</div>
 				</div>
 			</>
